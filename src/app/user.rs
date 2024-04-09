82e31ac31ffcca202390b4223beab889b29a941a
Piotr <114903054+elevenhsoft@users.noreply.github.com>
2024-04-09 17:39:29 +0200

    set user logged state only by param

--- src/app/user.rs
+++ src/app/user.rs
@@ -1,8 +1,6 @@
 use lemmy_api_common::sensitive::Sensitive;
 use serde_derive::{Deserialize, Serialize};
 
-use crate::settings::JWT;
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     pub username: Sensitive<String>,
@@ -11,12 +9,6 @@ pub struct User {
 
 impl User {
     pub fn new(username: Sensitive<String>, is_logged: bool) -> Self {
-        let is_logged = if let Ok(jwt) = confy::load::<JWT>("lemnux", "user") {
-            jwt.token.is_some()
-        } else {
-            is_logged
-        };
-
         Self {
             username,
             is_logged,
